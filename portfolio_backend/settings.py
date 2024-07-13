2cafb8bebc0703478218d44111d0047f0a05de8d
kalashsingh18 <kalashchouhan939@gmail.com>
2024-07-14 00:46:06 +0530

    secound commit

--- portfolio_backend/settings.py
+++ portfolio_backend/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key usedsecret!
 SECRET_KEY = 'django-insecure-_go=8o5*re40#o+f&prnfnl8xjgs3^l_p5d9ns!o0(@w+*8-ei'
 
 # SECURITY WARNING: don't run with debug turned on in production!
